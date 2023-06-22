31f366a79c5911f009daf9eb7a97c08bfd57f237
senya-koku <daiki.kokusenya.lp@ambl.co.jp>
2023-06-22 23:00:15 +0900

    fix

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # genepass
+
+generating passward randomly!
+
+# Install
+`pip install odach`
+
+# Usage
+
+```python
+import genepass
+
+genepass.create_random_password()
+```
+
+# Option
+
+- strong : DIGITS & CAPITAL_LETTER & SMALL_LETTER
+- god : DIGITS & CAPITAL_LETTER & SMALL_LETTER & SYMBOLS
+- custom : Under construction
--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 from setuptools import setup, find_packages
 
+readme = open('README.md').read()
+
 setup(
     name='genepass',
     version='0.1',
+    author=' ny7777',
+    author_email='meathouse47@gmail.com',
+    url='https://github.com/senya-koku/genepass',
+    description='genepass can automatically generates passwords',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    
     packages=find_packages()
 )
