c4d1cd7babfd8c72c480451ccdc99ab6f062caa2
senya-koku <daiki.kokusenya.lp@ambl.co.jp>
2023-07-03 12:48:55 +0900

    fix setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 readme = open('README.md').read()
 
-VERSION = '0.1.5'
+VERSION = '0.1.6'
 
 
 
@@ -10,9 +10,9 @@ setup(
     name='genepass',
     version=VERSION,
     author=' ny7777',
-    author_email='meathouse47@gmail.com',
+    author_email='5938dai617@gmail.com',
     url='https://github.com/senya-koku/genepass',
-    description='genepass can automatically generates passwords',
+    description='genepass can automatically generate (create) passwords',
     long_description=readme,
     long_description_content_type='text/markdown',
     license='MIT',
